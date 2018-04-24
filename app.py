9ec945f8eac181c3078259659533ea1cd827d793
Alexander Dobrzhansky <alexander.dobrzhansky@sigma.software>
2018-04-24 21:15:39 +0300

    feature task stub

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopmentConfig
+from models import Users
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Users
 
 
-@app.route('/users/<id>')
+@app.route('/users/<id>/')
 def hello_world(id):
     user = Users.query.get(id)
     return user.user_email
+
+
+@app.route('/users/create/')
+def create_user_view():
+    # todo create user with email form post body
+    pass
+
