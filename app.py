1236bbac6cc91770003e7eb1b7acea21f7580faa
Kirill Totok <kirill.totok@gmail.com>
2018-04-25 12:41:32 +0300

    Modified Users model to increment ID. Implement user creation

--- app.py
+++ app.py
@@ -16,28 +16,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 @app.route('/users/<id>/')
 def hello_world(id):
     user = Users.query.get(id)
+    print(user.user_email)
     return user.user_email
 
 
-@app.route('/users/create/', methods=['POST'])
+@app.route('/users/create', methods=['POST'])
 def create_user_view():
-    # todo create user with email form post body
-    new_user_email = request.form['user_email']
-    user_exists = Users.query.filter_by(user_email=new_user_email).first()
+    if request.form:
+        new_user_email = request.form['user_email']
+        user_exists = Users.query.filter_by(user_email=new_user_email).first()
 
-    if not user_exists:
-        new_user_model = Users(new_user_email)
-        db.session.add(new_user_model)
-        created_user = Users.query.filter_by(user_email=new_user_email).first()
-        return "New User crested: {} : {}"\
-            .format(created_user.id, created_user.user_email)
-    return "User already exists {} : {}".format(user_exists.id, user_exists.user_email)
+        if not user_exists:
+            new_user_model = Users(user_email=new_user_email)
+            db.session.add(new_user_model)
+            db.session.commit()
+            created_user = Users.query.filter_by(user_email=new_user_email).first()
+            return "New User crested: {} : {}"\
+                .format(created_user.id, created_user.user_email)
+        return "User already exists {} : {}".format(user_exists.id, user_exists.user_email)
 
 
 class Users(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_email = db.Column(db.String())
 
     def __repr__(self):
